8964edc21d84af0febff371b5b62022a7904ff61
@garaidipak1998 <garaidipak1998@gmail.com>
2021-08-28 19:23:20 +0530

    Class day132

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+// import heroes from './heroes';
+// import {Tachyons} from 'tachyons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+
+  constructor(){
+    console.log("Constructor Call....")
+    super();
+ 
+    this.state = {
+      data : [],
+      key : 0,
+      error : ''
+    }
+    console.log(this.state.data)
+
+  }
+  componetWillMount(){
+    console.log("ComponentWillMount Call .......")
+  }
+  // static getDerivedStateFromProps(){
+  //   console.log("getDerivedStateFromProps call...........")
+  // }
+
+  componentDidMount(){
+    // all backend endpoint call here
+    // console.log("ComponentDidMount  ...........");
+   
+    console.log(this.state.data);
+    this.fetchData()
+  }
+
+  fetchData = () =>{
+   
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        data : data
+      })
+
+    }).catch((e) =>{
+       console.log(e);
+       this.setState({
+         erro: e
+       })
+    });
+    
+    // this.setState({
+    //   data : data
+    // })
+    
+
+  }
+
+  render() {
+    
+    console.log("render function call ...")
+    return (
+      <>
+        <div className="app">
+          <h1>Learning Life Cycle of React</h1>
+        </div>
+        <div className="hero__cards">
+     {
+          this.state.data ? 
+          (this.state.data.map( (hero) => {
+            return(
+              <div className="card__item fl w-25">
+              <p>ID : {hero.id}</p>
+              <h3>Name : {hero.name}</h3>
+              </div>
+
+            )
+          })) : 
+        (
+          <div> {this.state.error}</div>
+        )
+        }
+      
+        </div>
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/heroes.js
@@ -0,0 +1,30 @@
+const  heroes = [
+    {
+        name : "Superman",
+        id:101
+    },
+    {
+        name : "Batman",
+        id:102
+    },
+    {
+        name : "Spiderman",
+        id:103
+    },
+    {
+        name : "Ironman",
+        id:104
+    },
+    {
+        name : "Thor",
+        id:105
+    },
+    
+    {
+        name : "Captain AMerica",
+        id:105
+    },
+    
+
+]
+export default heroes
